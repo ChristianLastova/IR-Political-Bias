96dfa1f92b18f2711183a23cfd58dd9c37c6f3f9
Christian Lastova <christianlastova@christians-mbp.lan>
2017-11-02 10:46:19 -0400

    Incomplete Selenium, still need to automate logging in a user as well

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,15 @@ package main;
 
 import java.io.FileReader;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
@@ -29,7 +33,7 @@ public class Main {
 		
 		for(String query : conservativeQueries) {
 			System.out.println(query);
-//			googleSearch(driver, query);
+			googleSearch(driver, query);
 //			bingSearch(driver, query);
 		}
 		
@@ -55,16 +59,29 @@ public class Main {
 		try{
 			driver.get("https://www.google.com"); //go to google.com
 			
+			//Selenium is erroring out when trying to click on first document after searching. WebDriverWait supposedly waits until the element is visible/click-able, 
+			//but was still causing problems last time I checked
+			WebDriverWait wait = new WebDriverWait(driver, 20);
+			
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.name("q")));
 			WebElement searchBox = driver.findElement(By.name("q")); //fill search field with query
 			searchBox.sendKeys(query);
-			//*[@id="rso"]/div[2]/div/div[1]/div/div/h3/a
+			searchBox.sendKeys(Keys.RETURN); // instead of finding a button, just hit enter. Was causing errors because original button was hidden by autocomplete suggestions
 			
-			WebElement searchButton = driver.findElement(By.name("btnK")); //click search
-			searchButton.click();
+			//XPATHs for first returned document and corresponding URL (not image, map, or ad. just link of first rank). 
+			//It is consistent because @id='rso' differentiates it from the other types of non-relevant documents
+			String docXPath = "//*[@id=\"rso\"]/div[2]/div/div[1]/div/div/h3/a";
+			String urlXPath = "//*[@id=\"rso\"]/div[2]/div/div[1]/div/div/div/div/div[1]/cite";
+
+//			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(docXPath)));
+//			WebElement firstDocument = driver.findElement(By.xpath(docXPath));
+//			String documentUrl = driver.findElement(By.xpath(urlXPath)).getText();
+//			System.out.println(documentUrl);
+//			firstDocument.click();
 		}catch(Exception e) {
 			System.out.println("Error searching query in Google: " + query);
 			e.printStackTrace(); //if there is an error, print the stack trace and close chrome
-			driver.quit();
+//			driver.quit();
 		}
 	}
 	
