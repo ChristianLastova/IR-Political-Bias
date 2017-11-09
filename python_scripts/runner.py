48e26a195ef257340fa01e16253c05908c8880d2
William <whg8hc@virginia.edu>
2017-11-09 11:43:48 -0500

    added clicking, started incorporating text analysis

--- python_scripts/runner.py
+++ python_scripts/runner.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import random
+import requests
+from bs4 import BeautifulSoup
+from sklearn.externals import joblib
 
 def getQueriesFromFile(filename):
 	with open(filename) as f:
@@ -15,9 +19,45 @@ def getQueriesFromFile(filename):
 
 def googleSearch(webdriver, query):
 	webdriver.get("http://google.com")
-	searchBox = webdriver.find_element_by_name("q"); 
+	searchBox = webdriver.find_element_by_name("q")
 	searchBox.send_keys(query)
 	searchBox.send_keys(Keys.ENTER)
+
+def googleSearchAndClick(webdriver, query):
+	webdriver.get("http://google.com")
+	searchBox = webdriver.find_element_by_name("q")
+	searchBox.send_keys(query)
+	searchBox.send_keys(Keys.ENTER)
+
+	time.sleep(3)
+
+	clf = joblib.load('bias_detector/political_bias_classifier_optimized.pkl')
+
+	try:
+		link = webdriver.find_element_by_xpath("//*[@id='rso']/div[1]/div/div/div/h3/a")
+		print("link: " + link.text)
+		url = webdriver.find_element_by_xpath("//*[@id='rso']/div[1]/div/div/div/div/div/div[1]/cite")
+		print("url: " + url.text)
+		ActionChains(webdriver).move_to_element(link).click().perform()
+		#download the page
+		page = requests.get(url.text)
+		#use BeautifulSoup to parse the html content of the page and extract the text
+		soup = BeautifulSoup(page.content, 'html.parser')
+		text = soup.get_text()
+		clf.predict([text])
+		print("--------------------------------")
+	except:
+		try:
+			link = webdriver.find_element_by_xpath("//*[@id='rso']/div/div/div[1]/div/div/h3/a")
+			print("link: " + link.text)
+			url = webdriver.find_element_by_xpath("//*[@id='rso']/div/div/div[1]/div/div/div/div/div/cite")
+			print("url: " + url.text)
+			ActionChains(webdriver).move_to_element(link).click().perform()
+			time.sleep(3)
+			print("--------------------------------")
+		except:
+			print("didn't find link")
+		
 
 def login(webdriver, email, password):
 	webdriver.get("http://google.com")
@@ -38,12 +78,15 @@ def login(webdriver, email, password):
 
 
 def main():
+
 	#array of tuples (username, password, queryset)
 	users = []
-	users.append(("joes9358", "controluser", "queries_controversial.txt"))
-	users.append(("janetheplummer123", "controltwo", "queries_controversial.txt"))
-	users.append(("js4425947", "liberaluser", "queries_liberal.txt"))
-	users.append(("jplum713", "conservativeuser", "queries_conservative.txt"))
+	#users.append(("joes9358", "controluser", "queries_controversial.txt"))
+	#users.append(("janetheplummer123", "controltwo", "queries_controversial.txt"))
+	#users.append(("js4425947", "liberaluser", "queries_liberal.txt"))
+	#users.append(("jplum713", "conservativeuser", "queries_conservative.txt"))
+
+	users.append(("moreese978", "controlclick", "queries_controversial.txt"))
 
 	for u in users:
 		queries = getQueriesFromFile("queries/" + u[2])
@@ -51,7 +94,7 @@ def main():
 
 		login(driver, u[0], u[1])
 		for q in queries:
-			googleSearch(driver, q)
+			googleSearchAndClick(driver, q)
 			time.sleep(5)
 
 		driver.quit()
